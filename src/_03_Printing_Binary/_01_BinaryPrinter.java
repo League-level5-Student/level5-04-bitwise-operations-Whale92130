3145b6f904f3eccd64efd89961f0a0bcd9ec173c
league <league@192.168.1.200>
2024-03-09 11:30:18 -0800

    worked on binary printer

--- src/_03_Printing_Binary/_01_BinaryPrinter.java
+++ src/_03_Printing_Binary/_01_BinaryPrinter.java
@@ -12,38 +12,51 @@ public class _01_BinaryPrinter {
         // We first want to print the bit in the one's place
     	System.out.println("Byte: " + b);
         // Shift b seven bits to the right
-    	int bb = b >> 7;
+    	int bb = b;
         // Use the & operator to "mask" the bit in the one's place
         // This can be done by "anding" (&) it with the value of 1
-    	System.out.println(bb);
+    	//System.out.println("bb: " + bb);
         // Print the result using System.out.print (NOT System.out.println)
-    	System.out.print(bb&1);
+    	//System.out.println("bb&1 " + (bb&1));
         //Use this method to print the remaining 7 bits of b
-    	for (int i = 1; i < 128; i*=(int) Math.pow(2, i)) {
-    		System.out.print(bb&i);
+    	String output ="";
+    	for (int i = 1<<7; i > 0; i=i>>1) {
+    		if ((bb&i) > 0) {
+    			output+="1";
+    		}
+    		else {
+    			output+="0";
+    		}
+    		//System.out.println("bb&" + i + " " + (bb&i));
     	}
+    	System.out.println("Final Byte: " + output);
     }
 
-    public void printShortBinary(short s) {
+    public static void printShortBinary(short s) {
         // Create 2 byte variables
-
         // Use bit shifting and masking (&) to save the first
         // 8 bits of s in one byte, and the second 8 bits of
         // s in the other byte
-
         // Call printByteBinary twice using the two bytes
         // Make sure they are in the correct order
+    	byte first8bits = (byte) (s>>8);
+    	byte last8bits = (byte) (s&(255));
+		printByteBinary(first8bits);
+    	printByteBinary(last8bits);
     }
 
-    public void printIntBinary(int i) {
+    public static void printIntBinary(int i) {
         // Create 2 short variables
 
         // Use bit shifting and masking (&) to save the first
         // 16 bits of i in one short, and the second 16 bits of
         // i in the other short
-
+    	short first16bits = (short) (i>>16);
+    	short last16bits = (short) (i&());
         // Call printShortBinary twice using the two short variables
         // Make sure they are in the correct order
+    	printShortBinary(first16bits);
+    	printShortBinary(last16bits);
     }
 
     public void printLongBinary(long l) {
@@ -52,7 +65,11 @@ public class _01_BinaryPrinter {
 
     public static void main(String[] args) {
         // Test your methods here
-    	byte b = 12;
-    	printByteBinary(b);
+    	byte b = 67;
+    	//printByteBinary(b);
+    	Short s = 513;
+    	//printShortBinary(s);
+    	int i = 2049;
+    	printIntBinary(i);
     }
 }
